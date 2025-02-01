4e09dcc2209fda22a54183fc715c362183779390
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2025-02-01 09:13:00 +0000

    create api key from google gemini

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [qeustion, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
+  async function GenerateAnswer() {
+    console.log("loading..");
+    const response = await axios({
+      method: "POST",
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBSqrvbA94-UhGjLPiCLAjE_SMYmWd7eqU",
+      data: {
+        contents: [{ parts: [{ text: qeustion }] }],
+      },
+    });
+    console.log(response.data.candidates[0].content.parts[0].text);
+  }
   return (
     <>
-      <h1>Chat-with-AI</h1>
+      <h1>Chat with AI</h1>
+      <textarea
+        value={qeustion}
+        onChange={(e) => setQuestion(e.target.value)}
+      ></textarea>
+      <button onClick={GenerateAnswer}>Generate Answer</button>
     </>
   );
 }
